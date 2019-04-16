2478cf72dcf16dcd4e2b196f8e8f2b11bd4c6268
David Van Beveren <david.vanbeveren@gmail.com>
2019-04-16 22:02:22 +0200

    Initial Site Files Upload

---
+++ mobile.php
@@ -0,0 +1,179 @@
+<html lang="en">
+<head>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-130853914-3"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-130853914-3');
+</script>
+
+<meta charset="UTF-8">
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<title>5 Minutes of Vegan Activism | 5minutes5vegans.org</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="theme-color" content="#3b5998">
+<link rel="manifest" href="https://5minutes5vegans.org/manifest.json">
+<link rel="apple-touch-icon" href="/images/icons-512.png">
+<meta name="apple-mobile-web-app-title" content="5M5V">
+<meta name="keywords" content="Vegan, Activism, Activist, Animal Rights, Animal Rights Activist, Vegan Activist, Vegan Activism, You Are Their Voice, YouAreTheirVoice, List, Guide, Get Active, How To, Resources, Speciesism, 5 Minutes 5 Vegans, 5m5v, Compassion, Animals, Animal Liberation, Plant Based">	
+<meta name="description" content="In just 5 minutes you can help 5 or more people become Vegan right now — our vegan robots are constantly scanning social media, they retweet people we think are looking for help with going Vegan!">
+<meta name="author" content="You Are Their Voice - https://youaretheirvoice.com">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="shortcut icon" href="/favicon.ico">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Fredoka+One">
+<link rel="stylesheet" href="https://5minutes5vegans.org/index.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script type="module" src="https://unpkg.com/x-frame-bypass"></script>
+<script src="//code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+</head>
+
+
+<div id="header">
+<h3 style="font-family: 'Fredoka One', cursive; font-weight: 100;margin-top: 40px;font-size: 28px;color:#fff;letter-spacing: 0.125em;">5minutes5vegans</h3>
+<p style="padding:0px 0px 0px 0px">In just 5 minutes you can help 5 or more people become Vegan right <i>now</i>— get started below!<br><br><span id="time">Get ready!</span> <a href="https://5minutes5vegans.com" name="Restart Timer" alt="Restart Timer" onclick="location.reload();"><img src="https://5minutes5vegans.org/images/restart.png" alt="Restart Timer" style="width:20px;"></a></p>
+</div>
+
+
+<div class="howdoesitwork" style="background-color: #dd3147;">
+<div id="flip"><center><h2 style="padding-left: 20px;padding-top: 5px;font-size: 20px;text-align: left;color:#fff;background-color:#3b5998;"><a href="#">How does it work? <img id="arrow1" src="https://5minutes5vegans.org/images/arrow.png" alt="Collapse Arrow" style="width:40px;float: right;padding-top: 8px;padding-right: 10px;"></h2></a></center></div>
+<div id="howdoesitwork">
+<p style="font-size: 16px;background-color: #dd3147;padding: 3px 20px 8px 20px;margin-top: 5px;">Our vegan robots are constantly scanning social media, they retweet people we think are looking for help with going Vegan. Below are a bunch of quick-copy resources you can reply with quickly if you'd like. Simply scroll further below to find people to support!</p>	
+</center>
+</div>
+</div> 
+
+<div id="flip2"><center><h2 style="padding-left: 20px;padding-top: 5px;font-size: 20px;text-align: left;color:#fff;background-color:#33a024;"><a href="#">Quick Responses! <img id="arrow2" src="https://5minutes5vegans.org/images/arrow.png" alt="Collapse Arrow" style="width:40px;float: right;padding-top: 8px;padding-right: 10px;"></h2></a></center></div>
+
+<div id="easycopyresources"> 
+<div class="tab">
+  <input type="button" class="topics" value="🤗 General Info" id="general">
+  <input type="button" class="topics" value="👊 Challenge 22+" id="22">
+  <input type="button" class="topics" value="😢 I Love Meat" id="meat">
+  <input type="button" class="topics" value="🧀 I Love Cheese" id="cheese">
+  <input type="button" class="topics" value="🍕 I Love Pizza" id="pizza">
+  <input type="button" class="topics" value="🥚 I Love Eggs" id="eggs">
+  <input type="button" class="topics" value="🍦 I Love Icecream" id="icecream">
+  <input type="button" class="topics" value="🐔 I Love Chicken" id="chicken">
+  <input type="button" class="topics" value="🥜 Plant Milks" id="milks">
+  <input type="button" class="topics" value="🎥 Documentaries" id="documentary">
+  <input type="button" class="topics" value="😥 Dominion Movie" id="dominion">
+  <input type="button" class="topics" value="👶 Baby Steps" id="baby">
+  <input type="button" class="topics" value="🥑 Vegetarian" id="vegetarian">
+  <input type="button" class="topics" value="💰 Too Expensive" id="expensive">
+  <input type="button" class="topics" value="🌱 Protein Issue" id="protein">
+  <input type="button" class="topics" value="🏋️ Fitness Info" id="fitness">
+  <input type="button" class="topics" value="👨 Community" id="community">
+  <input type="button" class="topics" value="🐄 Eating Out" id="eatingout">
+  <input type="button" class="topics" value="🌲 Environment" id="environment">
+  <input type="button" class="topics" value="👍 Everyone Agrees" id="agrees">
+  <input type="button" class="topics" value="💀 Is It Healthy" id="healthy">
+  <input type="button" class="topics" value="🥗 Lazy To Cook" id="cooking">
+  <input type="button" class="topics" value="🤦‍ Food is Boring" id="boring">
+  <input type="button" class="topics" value="👊‍ 30+ Arguments" id="common">
+</div>
+
+<table style="width: 100%;padding:7px 5px 2px 7px;background-color:#33a024;border-top: none;"><tr>
+<td style="width: 90%;padding: 5px 0px 5px 0px;">
+<div class="notice" style="text-align: left;color: #fff;letter-spacing: 1;padding-bottom: 8px;font-weight: 600;"><i class="fas fa-exclamation-triangle"></i> Please edit the text below to make it personal before using!</div>
+<textarea aria-label="Copy Text" id="txtarea" rows="5" cols="75%" style="width: 98%;border: none;padding: 5px 0px 0px 10px;margin-bottom: 4px;"></textarea></td>
+<td style="width: 10%;">
+<div class="tooltip">
+<button onclick="copyFunction()" onmouseout="outFunc()" style="background-color: #fff; color: #dd3147; width: 78px; height: 73px; font-weight: 700; letter-spacing: 0.125em; text-align: center; padding: 10px; border: 0px; border-radius: 25px; margin-right: 1px;">
+<span class="tooltiptext" id="copytooltip">Tap again</span>Copy
+</div>
+</td>
+</tr>
+</table>
+
+</div>
+
+
+<div class="iframetabs" style="top: 0;position: sticky;width: 100%;display:flex;justify-content:space-around;">
+<button aria-label="Twitter" class="iframebutton" onclick="openFrame(event, 'Twitter')" id="TwitterOpen" style="background-color:#4AB3F4;color:fff;width: 100%;"><i class="fab fa-twitter"></i></button>
+<button aria-label="Facebook" class="iframebutton" onclick="openFrame(event, 'Facebook')" style="background-color:#3b5998;color:fff;width: 100%;"><i class="fab fa-facebook-f"></i></button>
+<button aria-label="Reddit" class="iframebutton" onclick="openFrame(event, 'Reddit')" style="background-color:#ff4500;color:fff;width: 100%;"><i class="fab fa-reddit-alien"></i></button>
+<button aria-label="Instagram" class="iframebutton" onclick="openFrame(event, 'Instagram')" style="background-color:#e1306c;color:fff;width: 100%;"><i class="fab fa-instagram"></i></button>
+<button aria-label="Patreon" class="iframebutton" onclick="openFrame(event, 'Patreon')" style="background-color:#bf51ef;color:fff;width: 100%;"><i class="fas fa-hand-holding-heart"></i></button>
+</div>
+
+<!-- load twitter -->
+<div id="Twitter" class="iframecontent">
+<div style="width: 100%;padding: 5px 0px 5px 10px;  text-align:left;background-color: #4AB3F4; color:fff;">
+<i class="fas fa-info-circle"></i>&nbsp;&nbsp;<b>VegAssist</b> retweets folks we think need help!
+</div>
+<a class="twitter-timeline" href="https://twitter.com/VegAssist"></a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+</div>
+
+<!-- load facebook -->
+<div id="Facebook" class="iframecontent" style="height: 100vh;">
+<div style="width: 100%;padding: 5px 0px 5px 10px;  text-align:left;background-color: #3b5998; color:fff;">
+<i class="fas fa-info-circle"></i>&nbsp;&nbsp;<b>VegAssistFB</b> reposts folks we think need help!
+</div>
+<center><img src="https://5minutes5vegans.org/images/coming-soon.png" alt="Coming Soon" style="width:60%;"></center>
+</div>
+
+<!-- load reddit -->
+<div id="Reddit" class="iframecontent" style="height: 100vh;">
+<div style="width: 100%;padding: 5px 0px 5px 10px;  text-align:left;background-color: #ff4500; color:fff;">
+<i class="fas fa-info-circle"></i>&nbsp;&nbsp;<b>/r/Vegan</b> thread with folks we think need help!
+</div>
+<iframe is="x-frame-bypass" src="https://www.reddit.com/r/vegan/comments/aumtok/random_thoughts_small_questions_and_general/?sort=new" style="width:100%; height:100vh;" frameborder="0"></iframe>
+</div>
+
+<!-- load instagram -->
+<div id="Instagram" class="iframecontent" style="height: 100vh;">
+<div style="width: 100%;padding: 5px 0px 5px 10px;  text-align:left;background-color: #e1306c; color:fff;">
+<i class="fas fa-info-circle"></i>&nbsp;&nbsp;<b>VegAssist</b> hashtags folks we think need help!
+</div>
+<center><img src="https://5minutes5vegans.org/images/coming-soon.png" alt="Coming Soon" style="width:60%;"></center>
+</div>
+
+<!-- load patreon -->
+<div id="Patreon" class="iframecontent" style="background-color: #ffc83d;">
+<div style="width: 100%;background-color: #fff;"><br>
+<center><img src="https://5minutes5vegans.org/images/yatv-logo.png" alt="You Are Their Voice Logo" style="width: 350px;"></center>
+<br>
+</div>
+<h2 style="color:#fff;background-color:#e6461a;">Support on Patreon!</h2>
+
+<table style="width:100%; background-color:#ffc83d;">
+<tr>
+<td style="width: 100%;"><p style="background-color: #ffffff;padding: 0px 20px 5px 20px;margin-top: 14px;border-radius: 25px;color: #000;padding: 22px;margin: 32px;">
+Whoa! Thank you so much for taking an interest in supporting us & our activism! We really appreciate it. ❤️<br><br>
+This project is one of many from <a href="http://YouAreTheirVoice.com" target="_blank" style="color: #e6461a;">YouAreTheirVoice.com</a> and we're 100% funded by <a href="https://www.patreon.com/youaretheirvoice" target="_blank" style="color: #e6461a;">our Patreon</a> supporters. It doesn't matter whether it' $1 or $5, any amount is appreciated beyond words... plus you also get goodies!
+<br><br>
+Special thanks to <a href="http://YourVeganFallacyIs.com" target="_blank" style="color: #e6461a;">YourVeganFallacyIs</a> for lending his coding knowledge, and more importantly, his time. Last but not least a very special thanks to <a href="https://twitter.com/plorry" target="_blank" style="color: #e6461a;">@Plorry</a> (Andrew Gardner) for his bot <a href="https://twitter.com/vegassist" target="_blank" style="color: #e6461a;">@VegAssist</a>!
+<br>
+<center><a href="https://www.patreon.com/youaretheirvoice" target="_blank"><img src="https://5minutes5vegans.org/images/patreon-button.png" alt="Patreon Donation Button" style="width:60%"></a></center>
+<br>
+</p>
+</td>
+</tr>
+</table>
+</div>
+
+
+<div id="flip3"><center><h2 style="padding-left: 20px;padding-top: 5px;font-size: 20px;text-align: left;color:#fff;background-color:#7f1b1b;"><a href="#">Thanks for helping! <img id="arrow3" src="https://5minutes5vegans.org/images/arrow.png" alt="Collapse Arrow" style="width:40px;float: right;padding-top: 8px;padding-right: 10px;"></h2></a></center></div>
+<div id="thankyou" style="background-color: #dd3147;color: #fff;">
+<table style="width: 100%;">
+  <tr>
+    <td style="width:50%;text-align: center;"><p style="font-size: 16px;padding: 0px 5px 5px 5px;margin-top: 4px;">Contribute even more in the world of Activism!<br> <a href="https://VeganActivism.org" target="_blank"><u>VeganActivism.org</u></a></p></td>
+    <td style="width:50%;text-align: center;"><p style="font-size: 16px;padding: 0px 5px 5px 5px;margin-top: 4px;">Find even more Activist tools & resources here!<br> <a href="https://YouAreTheirVoice.com" target="_blank"><u>YouAreTheirVoice.com</u></a></p></td>
+  </tr>
+</table>
+</div>
+
+<!-- bottom notice bar-->
+<div style="bottom: -1;width: 100%;padding:10px 19px 10px 19px; text-align:center;  background-color: #3b5998; color:fff;">
+<i class="fas fa-exclamation-triangle"></i>&nbsp;&nbsp;<b>Dont see anything?</b> Disable browser extensions that could be blocking our scripts!<div class="space" style="padding-top: 10px;"><i class="fas fa-info-circle"></i>&nbsp;&nbsp;<a href="https://youaretheirvoice.com/contact?5m5v" target="_blank"><u>Contact us</u></a> or <a href="https://www.patreon.com/youaretheirvoice" target="_blank"><u>Donate</u></a>.</div>
+</div>
+
+<script src="/scripts.js"></script>
+
+<script type="text/javascript" src="//downloads.mailchimp.com/js/signup-forms/popup/unique-methods/embed.js" data-dojo-config="usePlainJson: true, isDebug: false"></script><script type="text/javascript">window.dojoRequire(["mojo/signup-forms/Loader"], function(L) { L.start({"baseUrl":"mc.us20.list-manage.com","uuid":"0baba35be8f6397f7ac1066f1","lid":"5fd11d4221","uniqueMethods":true}) })</script>
+
+</html>
---
+++ scripts.js
@@ -0,0 +1,194 @@
+$(document).ready(function(){
+  $("#icecream").click(function(){
+      $('#txtarea').val('Check out Ben & Jerry\'s Non-Dairy Icecream here: https://www.benjerry.com/flavors/non-dairy They have a lot of different flavors and there\'s a lot of other soy/almond/coconut based icecream brands too! See: https://www.vegan.com/ice-cream/ #vegan #5m5v');
+  });
+  $("#meat").click(function(){
+      $('#txtarea').val('Vegan meat has come a LONG way in just the past few years! Beyond Meat is really good: https://www.beyondmeat.com/, and Gardein also has a ton of products: https://tinyurl.com/gardeinmeat Take a look around your grocery store to see what\'s available, you might be surprised! #vegan #5m5v');
+  });
+  $("#chicken").click(function(){
+      $('#txtarea').val('Gardein makes some incredible chicken: https://tinyurl.com/gardeinmeat, look around at Whole Foods or other stores! HappyCow to eat out: https://www.happycow.net! Take a look around your grocery store to see what\'s available, you might be surprised! #vegan #5m5v');
+  });
+  $("#cheese").click(function(){
+      $('#txtarea').val('Vegan cheese has come a LONG way in just the past few years! My favorite is "Follow Your Heart" cheese, and Daiya is great for melted cheese. Use this link https://tinyurl.com/vegancheesenow to find even more brands near you! Message me if you need help finding more. #vegan #5m5v');
+  });
+  $("#dominion").click(function(){
+      $('#txtarea').val('I highly recommend this Youtube video: https://youtu.be/ny6aqdFy9SI?t=43, If you have the time, the first 15 minutes of the video is pretty life-changing. Message me back if you need any help going Vegan and I\'d be happy to help or answer any questions you may have! #vegan #5m5v');
+  });
+  $("#pizza").click(function(){
+      $('#txtarea').val('There\'s usually a lot of Vegan pizza options (and other restaurants) on http://HappyCow.net! There\'s also "Daiya" Pizza: https://daiyafoods.com/our-foods/pizza/ and even more places listed here: https://tinyurl.com/vegpizzanow Message me if you need help finding more. #vegan #5m5v');
+  });
+  $("#expensive").click(function(){
+      $('#txtarea').val('Vegan is actually really cheap, only expensive if you buy all the fancy "sold to vegans" stuff! Potatoes, Rice, Tofu, Beans, Lentils, Veggies, Fruits, Pasta, Oats, are incredibly cheap, especially in bulk! This guy has a great video on it: https://www.youtube.com/watch?v=Vs_nXVmyP1E #vegan #5m5v');
+  });
+  $("#eggs").click(function(){
+      $('#txtarea').val('There\'s a lot of really good Vegan egg recipes (like this one, but add spinach): https://tinyurl.com/eggtofu You can buy Vegan eggs here too: https://tinyurl.com/buyveganegg And even more info here: http://bit.ly/2rxeYl0 and especially here: https://tinyurl.com/vivaeggs #vegan #5m5v');
+  });
+  $("#documentary").click(function(){
+      $('#txtarea').val('Here are some movies for motivation! Cowspiracy (Environment): https://tinyurl.com/cownetflix Forks Over Knifes (Health): https://tinyurl.com/forksnetflix Dominion (Animals): https://youtu.be/ny6aqdFy9SI?t=43 Let me know if you watch any of them! 🌱 #vegan #5m5v');
+  });
+  $("#22").click(function(){
+      $('#txtarea').val('You can do it! 🐷 You should check out the Vegan 22+ day Challenge! They give you a free personal mentor on Facebook to help you go Vegan for a month. The link is: https://www.challenge22.com/challenge22; Good luck, you got this! 🐮 Message me back if you need any help. #vegan #5m5v');
+  });
+  $("#milks").click(function(){
+      $('#txtarea').val('Check out "Dairy is Scary", it\'s only 5 minutes long: https://youtu.be/UcN7SGGoCNI There\'s also tasty plant milks now like: Soy, Almond, Oat, Coconut, Rice & Cashew milk! These milks don\'t get gross or bad, they\'re healthier, and no refrigeration is needed before opening. #vegan #5m5v');
+  });
+  $("#baby").click(function(){
+      $('#txtarea').val('It\'s awesome that you\'re interested in being Vegan! 🐮 If it\'s too overwhelming and you don\'t know what to do, try taking baby steps. First just start buying soy/almond/oat milks and get used to that. Then do butter, meats, etc. More info: http://earthlinged.com/ebook #vegan #5m5v');
+  });
+  $("#vegetarian").click(function(){
+      $('#txtarea').val('Vegetarian is awesome! But I\'d really recommend you check this video out: https://www.youtube.com/watch?v=uWna6-niYEg Here\'s some common arguments debunked: http://bit.ly/ebookearthlingedsurge and here\'s the 1 month vegan challenge: https://www.challenge22.com/ #vegan #5m5v');
+  });
+  $("#common").click(function(){
+      $('#txtarea').val('Actually that\'s what I thought at first too haha! Here\'s that argument and other common arguments debunked: http://bit.ly/ebookearthlingedsurge and there\'s also a playlist on youtube on it: https://www.youtube.com/playlist?list=PL03LZR09P2gQJyBgHk_XE8gbj8j9uFs8G #vegan #5m5v');
+  });
+  $("#protein").click(function(){
+      $('#txtarea').val('Almost all foods, vegetables, fruits, seeds, all have protein, which we don\'t need a lot of. Here\'s a link that goes over the protein/meat myth: https://www.vivahealth.org.uk/resources/protein-myth-fact-sheet and video: https://www.youtube.com/watch?v=1elt5YCRLbk #vegan #5m5v');
+  });
+  $("#fitness").click(function(){
+      $('#txtarea').val('The worlds strongest man is a Vegan and there\'s so many more world records in fitness/strength held by Vegans! Working out on a plant-based diet is better for your training. Videos: https://www.youtube.com/results?search_query=vegan+fitness and Community: https://www.reddit.com/r/veganfitness/ #vegan #5m5v');
+  });
+  $("#community").click(function(){
+      $('#txtarea').val('If you need people to talk to, or just get support/help, check out http://reddit.com/r/Vegan! It\'s a community of around 300,000 Vegans that know the struggle and have the experience, ask them anything. There\'s also /r/VeganFitness, /r/VeganRecipes, and more! #vegan #5m5v');
+  });
+  $("#eatingout").click(function(){
+      $('#txtarea').val('Eating out is sometimes hard when you don\'t know where to go or what to eat but it does get much easier as time goes on & pretty soon you\'ll be an expert on what to get. Until then check this site out: http://happycow.net for Vegan food around your area! 🐮 #vegan #5m5v');
+  });
+  $("#environment").click(function(){
+      $('#txtarea').val('There\'s really daunting statistics on what Meat and Dairy industry (Animal Agriculture) is doing to the Environment, all backed up with sources & studies: https://youaretheirvoice.com/pages/the-daunting-facts Example: Animal agriculture is responsible for up to 91% of Amazon destruction. #vegan #5m5v');
+  });
+  $("#agrees").click(function(){
+      $('#txtarea').val('In today\'s age it\'s widely accepted that Veganism is healthier than a meat-eating diet. Here\'s a page that pulls sources directly from the world\'s largest Health, Nutrition and Dietary organizations (so from non-biased organizations): https://youaretheirvoice.com/pages/the-clear-consensus #vegan #5m5v');
+  });
+  $("#healthy").click(function(){
+      $('#txtarea').val('In today\'s age it\'s widely accepted that Meat & Dairy is extremely unhealthy compared to a Vegan diet. Meat is carcinogenic, the cause of the majority diseases, cancer, heart disease & diabetes, and obesity. Here\'s a page with info & sources: https://youaretheirvoice.com/pages/the-clear-consensus #vegan #5m5v');
+  });
+  $("#cooking").click(function(){
+      $('#txtarea').val('A lot of Vegans don\'t cook either, just buy the Vegan options at the store or at restaurants instead of the animal options. You don\'t need to learn to cook to eat healthy as a Vegan! Eating out is also really easy with http://happycow.net 🐮 Message me for help! #vegan #5m5v');
+  });
+  $("#boring").click(function(){
+      $('#txtarea').val('Pretty much anything made with meat/dairy can be replicated and be just as awesome, you just have to learn where to buy it or cook it. Here\'s some recipes: https://veganuary.com/recipes (or google your favorite food + "Vegan"). This for eating out: https://happycow.net #vegan #5m5v');
+  });
+  $("#common").click(function(){
+      $('#txtarea').val('That\'s a common myth! Check this out: http://earthlinged.com/ebook; it basically goes over the 30 most common myths about Veganism. Video format here: https://tinyurl.com/y862x5sg Message me back if you need any help or have any questions! #vegan #5m5v');
+  });
+  $("#general").click(function(){
+      $('#txtarea').val('You should do it! Most vegans say the only regret they have is that they didn\'t do it earlier. Here\'s some resources: http://reddit.com/r/Vegan (community) https://youtu.be/ny6aqdFy9SI?t=41 (Movie) https://www.youtube.com/playlist?list=PL03LZR09P2gQJyBgHk_XE8gbj8j9uFs8G (Myths) 🐷 Message me if you need help! #vegan #5m5v');
+  });
+  document.getElementById("22").click();
+});
+
+$(document).ready(function(){
+  $("#flip").click(function(){
+    $("#howdoesitwork").slideToggle("slow");
+	$("#arrow1").toggleClass("flip-vertical");
+  });
+});
+
+$(document).ready(function(){
+  $("#flip2").click(function(){
+    $("#easycopyresources").slideToggle("slow");
+	$("#arrow2").toggleClass("flip-vertical");
+  });
+});
+
+$(document).ready(function(){
+  $("#flip3").click(function(){
+    $("#thankyou").slideToggle("slow");
+	$("#arrow3").toggleClass("flip-vertical");
+  });
+});
+
+$(function() {
+  var A = parseInt($('#leftside').width(), 10),
+    B = parseInt($('#rightside').width(), 10),
+    Z = parseInt($('#draggablediv').width(), 10),
+    minw = parseInt((A + B + Z) * 10 / 100, 10),
+    offset = $('#container').offset(),
+    splitter = function(event, ui) {
+      var aw = parseInt(ui.position.left),
+          bw = A + B - aw;
+  
+      var leftSideCSS = {width : aw},
+        rightSideCSS = {width : bw},
+        $leftSide = $('#leftside'),
+        $rightSide = $('#rightside');
+  
+  
+      if (1329 > $leftSide.width()) {
+        leftSideCSS['z-index'] = 0;
+        rightSideCSS['z-index'] = 1;
+      }
+      else {
+        leftSideCSS['z-index'] = 1;
+        rightSideCSS['z-index'] = 0;
+      }
+  
+      $leftSide.css(leftSideCSS);
+      $rightSide.css(rightSideCSS);
+  
+  
+    };
+  $('#draggablediv').draggable({
+    iframeFix: true,
+    axis : 'x',
+    containment : [
+      offset.left + minw,
+      offset.top,
+      offset.left + A + B - minw,
+      offset.top + $('#container').height()
+    ],
+    drag : splitter
+  });
+});
+
+function copyFunction() {
+  var copyText = document.getElementById("txtarea");
+  copyText.select();
+  document.execCommand("copy");
+  
+  var tooltip = document.getElementById("copytooltip");
+  tooltip.innerHTML = "Copied! ";
+}
+
+function outFunc() {
+  var tooltip = document.getElementById("copytooltip");
+  tooltip.innerHTML = "Copy...";
+}
+
+function openFrame(evt, cityName) {
+  var i, iframecontent, iframebutton;
+  iframecontent = document.getElementsByClassName("iframecontent");
+  for (i = 0; i < iframecontent.length; i++) {
+    iframecontent[i].style.display = "none";
+  }
+  iframebutton = document.getElementsByClassName("iframebutton");
+  for (i = 0; i < iframebutton.length; i++) {
+    iframebutton[i].className = iframebutton[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+document.getElementById("TwitterOpen").click();
+
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10)
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+window.onload = function () {
+    var fiveMinutes = 60 * 5,
+	display = document.querySelector('#time');
+    startTimer(fiveMinutes, display);
+};
