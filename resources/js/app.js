040592e503b914f6b3f776cda02c6fcf30712354
Pablo Kebees <pablopetje@gmail.com>
2019-08-01 13:54:41 +0200

    Update I made ages ago, god knows what

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,9 +4,11 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
+require("./bootstrap");
 
-window.Vue = require('vue');
+require("./main");
+
+window.Vue = require("vue");
 
 /**
  * The following block of code may be used to automatically register your
@@ -19,7 +21,10 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i);
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component(
+    "example-component",
+    require("./components/ExampleComponent.vue").default
+);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -28,5 +33,5 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  */
 
 const app = new Vue({
-    el: '#app',
+    el: "#app"
 });
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,99 +1,199 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang="en">
+<head>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-130853914-3"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-        <title>Laravel</title>
+  gtag('config', 'UA-130853914-3');
+</script>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+<meta charset="UTF-8">
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<title>5 Minutes of Vegan Activism | 5minutes5vegans.org</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="theme-color" content="#3b5998">
+<link rel="manifest" href="https://5minutes5vegans.org/manifest.json">
+<link rel="apple-touch-icon" href="/images/icons-512.png">
+<meta name="apple-mobile-web-app-title" content="5M5V">
+<meta name="keywords" content="Vegan, Activism, Activist, Animal Rights, Animal Rights Activist, Vegan Activist, Vegan Activism, You Are Their Voice, YouAreTheirVoice, List, Guide, Get Active, How To, Resources, Speciesism, 5 Minutes 5 Vegans, 5m5v, Compassion, Animals, Animal Liberation, Plant Based">
+<meta name="description" content="In just 5 minutes you can help 5 or more people become Vegan right now — our vegan robots are constantly scanning social media, they retweet people we think are looking for help with going Vegan!">
+<meta name="author" content="You Are Their Voice - https://youaretheirvoice.com">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="shortcut icon" href="/favicon.ico">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Fredoka+One">
+<link rel="stylesheet" href="{{ asset('css/app.css') }}">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script type="module" src="https://unpkg.com/x-frame-bypass"></script>
+<script src="//code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<script src="{{ asset('js/app.js') }}"></script>
+</head>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
 
-            .full-height {
-                height: 100vh;
-            }
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+<div id='container'>
 
-            .position-ref {
-                position: relative;
-            }
+<!-- START leftside CONTENT HERE -->
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+<div id='leftside' class="leftside">
 
-            .content {
-                text-align: center;
-            }
+<div id="header">
+<h3 class="headertext">5minutes5vegans.org</h3>
+<p style="padding:0px 0px 0px 0px">In just 5 minutes you can help 5 or more people become Vegan right <i>now</i>— get started below!<br><br><span id="time">Get ready!</span> <a href="https://5minutes5vegans.com" name="Restart Timer" alt="Restart Timer" onclick="location.reload();"><img src="https://5minutes5vegans.org/images/restart.png" alt="Restart Timer" style="width:20px;"></a></p>
+</div>
 
-            .title {
-                font-size: 84px;
-            }
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
+<div class="howdoesitwork" style="background-color: #dd3147;">
+<div id="flip"><center><h2 style="color:#fff;background-color:#3b5998;"><a href="#">How does it work? <img id="arrow1" src="https://5minutes5vegans.org/images/arrow.png" alt="Collapse Arrow" style="width:40px;float: right;padding-top: 12px;padding-right: 10px;"></h2></a></center></div>
+<div id="howdoesitwork">
+<p style="background-color: #dd3147;padding: 0px 20px 5px 20px;margin-top: 14px;">Our vegan robots are constantly scanning social media, they retweet people we think are looking for help with going Vegan. Below are a bunch of quick-copy resources you can reply with quickly if you'd like. Simply scroll through the lists on the right and talk to the ones that could use a little help!</p>
+</center>
+</div>
+</div>
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+<div id="flip2"><center><h2 style="color:#fff;background-color:#33a024;"><a href="#">Easy to copy resources! <img id="arrow2" src="https://5minutes5vegans.org/images/arrow.png" alt="Collapse Arrow" style="width:40px;float: right;padding-top: 12px;padding-right: 10px;"></h2></a></center></div>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+<div id="easycopyresources">
+<div class="tab">
+  <input type="button" class="topics" value="🤗 General Info" id="general">
+  <input type="button" class="topics" value="👊 Challenge 22+" id="22">
+  <input type="button" class="topics" value="😢 I Love Meat" id="meat">
+  <input type="button" class="topics" value="🧀 I Love Cheese" id="cheese">
+  <input type="button" class="topics" value="🍕 I Love Pizza" id="pizza">
+  <input type="button" class="topics" value="🥚 I Love Eggs" id="eggs">
+  <input type="button" class="topics" value="🍦 I Love Icecream" id="icecream">
+  <input type="button" class="topics" value="🐔 I Love Chicken" id="chicken">
+  <input type="button" class="topics" value="🥜 Plant Milks" id="milks">
+  <input type="button" class="topics" value="🎥 Documentaries" id="documentary">
+  <input type="button" class="topics" value="😥 Dominion Movie" id="dominion">
+  <input type="button" class="topics" value="👶 Baby Steps" id="baby">
+  <input type="button" class="topics" value="🥑 Vegetarian" id="vegetarian">
+  <input type="button" class="topics" value="💰 Too Expensive" id="expensive">
+  <input type="button" class="topics" value="🌱 Protein Issue" id="protein">
+  <input type="button" class="topics" value="🏋️ Fitness Info" id="fitness">
+  <input type="button" class="topics" value="👨 Community" id="community">
+  <input type="button" class="topics" value="🐄 Eating Out" id="eatingout">
+  <input type="button" class="topics" value="🌲 Environment" id="environment">
+  <input type="button" class="topics" value="👍 Everyone Agrees" id="agrees">
+  <input type="button" class="topics" value="💀 Is It Healthy" id="healthy">
+  <input type="button" class="topics" value="🥗 Lazy To Cook" id="cooking">
+  <input type="button" class="topics" value="🤦‍ Food is Boring" id="boring">
+  <input type="button" class="topics" value="👊‍ 30+ Arguments" id="common">
+</div>
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+<table class="textareatable"><tr>
+<td style="width: 90%;padding: 5px 0px 5px 0px;">
+<div class="notice"><i class="fas fa-exclamation-triangle"></i> Please edit the text below to make it personal before using!</div>
+<textarea aria-label="Copy Text" id="txtarea" rows="5" cols="75%"></textarea></td>
+<td style="width: 10%;">
+<div class="tooltip">
+<button class="selecttext" onclick="copyFunction()" onmouseout="outFunc()">
+<span class="tooltiptext" id="copytooltip">Copy...</span>Copy
+</div>
+</td>
+</tr>
+</table>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
-    </body>
+</div>
+
+<div id="flip3"><center><h2 style="color:#fff;background-color:#7f1b1b;"><a href="#">Thanks for your time! <img id="arrow3" src="https://5minutes5vegans.org/images/arrow.png" alt="Collapse Arrow" style="width:40px;float: right;padding-top: 12px;padding-right: 10px;"></h2></a></center></div>
+<div id="thankyou">
+<table style="width: 100%;">
+  <tr>
+    <td style="width:50%;text-align: center;"><p style="font-size: 17px;padding: 0px 5px 5px 5px;margin-top: 4px;">Want to contribute even more in the world of Activism? Check out our other project:<br> <a href="https://VeganActivism.org" target="_blank"><u>VeganActivism.org</u></a>!&nbsp;&nbsp;<i class="fas fa-sign-out-alt"></i></p></td>
+    <td style="width:50%;text-align: center;"><p style="font-size: 17px;padding: 0px 5px 5px 5px;margin-top: 4px;">Find more tools and resources for Vegan Activism by visiting our site at:<br> <a href="https://YouAreTheirVoice.com" target="_blank"><u>YouAreTheirVoice.com</u></a>!&nbsp;&nbsp;<i class="fas fa-sign-out-alt"></i></p></td>
+  </tr>
+</table>
+</div>
+
+</div>
+
+<!-- START rightside CONTENT HERE -->
+
+<div id='rightside'>
+
+<div class="iframetabs" style="top: 0;position: sticky;width: 100%;display:flex;justify-content:space-around;">
+<button aria-label="Twitter" class="iframebutton" onclick="openFrame(event, 'Twitter')" id="TwitterOpen" style="background-color:#4AB3F4;color:fff;width: 100%;"><i class="fab fa-twitter"></i>&nbsp;&nbsp;Twitter</button>
+<button aria-label="Facebook" class="iframebutton" onclick="openFrame(event, 'Facebook')" style="background-color:#3b5998;color:fff;width: 100%;"><i class="fab fa-facebook-f"></i>&nbsp;&nbsp;Facebook</button>
+<button aria-label="Reddit" class="iframebutton" onclick="openFrame(event, 'Reddit')" style="background-color:#ff4500;color:fff;width: 100%;"><i class="fab fa-reddit-alien"></i>&nbsp;&nbsp;Reddit</button>
+<button aria-label="Instagram" class="iframebutton" onclick="openFrame(event, 'Instagram')" style="background-color:#e1306c;color:fff;width: 100%;"><i class="fas fa-envelope"></i>&nbsp;&nbsp;Newsletter</button>
+<button aria-label="Patreon" class="iframebutton" onclick="openFrame(event, 'Patreon')" style="background-color:#33a024;color:fff;width: 100%;"><i class="fas fa-hand-holding-heart"></i>&nbsp;&nbsp;Support Us</button>
+</div>
+
+<!-- load twitter -->
+<div id="Twitter" class="iframecontent">
+<div class="twitternotice">
+<i class="fas fa-info-circle"></i>&nbsp;&nbsp;<b>VegAssist</b> retweets people we think need your help!&nbsp;&nbsp;<i class="fas fa-user"></i>&nbsp;&nbsp;<a href="https://twitter.com/i/flow/signup" target="_blank"><u>Register for Twitter</u></a> and <a href="http://plorry.github.io/VegAssist/" target="_blank"><u>read this first</u></a> before starting!
+</div>
+<a class="twitter-timeline" href="https://twitter.com/VegAssist"></a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+</div>
+
+<!-- load facebook -->
+<div id="Facebook" class="iframecontent" style="height: 100vh;">
+<div class="fbnotice">
+<i class="fas fa-info-circle"></i>&nbsp;&nbsp;<b>VegAssistFB</b> shares posts from people we think need your help!&nbsp;&nbsp;<i class="fas fa-user"></i>&nbsp;&nbsp;<a href="https://www.facebook.com/" target="_blank"><u>Register for a facebook.com account</u></a>!
+</div>
+<center><img src="https://5minutes5vegans.org/images/coming-soon.png" alt="Coming Soon" style="width:40%;"></center>
+</div>
+
+<!-- load reddit -->
+<div id="Reddit" class="iframecontent" style="height: 100vh;">
+<div class="redditnotice">
+<i class="fas fa-info-circle"></i>&nbsp;&nbsp;<b>/r/Vegan</b> shows a thread with people we think need your help!&nbsp;&nbsp;<i class="fas fa-user"></i>&nbsp;&nbsp;<a href="https://www.reddit.com/" target="_blank"><u>Register for a reddit.com account</u></a>!
+</div>
+<iframe is="x-frame-bypass" src="https://www.reddit.com/r/vegan/comments/b8gonx/random_thoughts_small_questions_and_general/?sort=new" style="width:100%; height:100vh;" frameborder="0"></iframe>
+</div>
+
+<!-- load tumblr -->
+<div id="Instagram" class="iframecontent" style="height: 100vh;">
+<iframe src="https://youaretheirvoice.us20.list-manage.com/subscribe?u=0baba35be8f6397f7ac1066f1&id=5fd11d4221" style="width:100%; height:100vh;" frameborder="0"></iframe>
+</div>
+
+<!-- load patreon -->
+<div id="Patreon" class="iframecontent" style="height: 100vh;background-color: #ffc83d;">
+<div style="width: 100%;background-color: #fff;"><br>
+<center><img src="https://5minutes5vegans.org/images/yatv-logo.png" alt="You Are Their Voice Logo" style="width: 350px;"></center>
+<br>
+</div>
+<h2 style="color:#fff;background-color:#e6461a;">Support our Activism on Patreon! <img id="arrow" src="https://5minutes5vegans.org/images/arrow.png" alt="Collapse Arrow" style="width:40px;float: right;padding-top: 12px;padding-right: 10px;"></h2>
+
+<table style="width:100%; background-color:#ffc83d;">
+<tr>
+<td style="width: 60%;"><p style="margin-top: 40px !important;background-color: #ffffff;padding: 0px 20px 5px 20px;margin-top: 14px;border-radius: 25px;color: #000;padding: 32px;margin: 52px;">
+Whoa! Thank you so much for taking an interest in supporting us & our activism! We really appreciate it. ❤️<br><br>
+This project is one of many from <a href="http://YouAreTheirVoice.com" target="_blank" style="color: #e6461a;">YouAreTheirVoice.com</a> and we're 100% funded by <a href="https://www.patreon.com/youaretheirvoice" target="_blank" style="color: #e6461a;">our Patreon</a> supporters. It doesn't matter whether it' $1 or $5, any amount is appreciated beyond words... plus you also get goodies!
+<br><br>
+Special thanks to <a href="http://YourVeganFallacyIs.com" target="_blank" style="color: #e6461a;">YourVeganFallacyIs</a> for lending his coding knowledge, and more importantly, his time. Last but not least a very special thanks to <a href="https://twitter.com/plorry" target="_blank" style="color: #e6461a;">@Plorry</a> (Andrew Gardner) for his bot <a href="https://twitter.com/vegassist" target="_blank" style="color: #e6461a;">@VegAssist</a>!
+<br>
+<center style="margin-top: -20px;"><a href="https://www.patreon.com/youaretheirvoice" target="_blank"><img src="https://5minutes5vegans.org/images/patreon-button.png" alt="Patreon Donation Button"></a></center>
+<br>
+</p>
+</td>
+<td style="width: 40%;padding: 32px;margin: 52px;">
+<center><img src="https://5minutes5vegans.org/images/children-pig.png" alt="Children hugging pig" style="border-radius: 25px;"></center>
+</td>
+</tr>
+</table>
+</div>
+
+<!-- bottom notice bar-->
+<div style="position: sticky;bottom: -1;width: 100%;padding: 5px 0px 5px 0px;  background-color: #3b5998; color:fff;">
+<i class="fas fa-exclamation-triangle"></i>&nbsp;&nbsp;<b>Dont see anything?</b> Disable browser extensions that could be blocking our scripts!&nbsp;&nbsp;<i class="fas fa-info-circle"></i>&nbsp;&nbsp;<a href="https://youaretheirvoice.com/contact?5m5v" target="_blank"><u>Contact</u></a> or <a href="https://www.patreon.com/youaretheirvoice" target="_blank"><u>Donate</u></a>.
+</div>
+
+</div>
+<!-- END rightside CONTENT HERE -->
+
+<div id='draggablediv'><img src='/images/right.png' style="padding-left: 3px;position: fixed;top: 5px;"><img src='/images/left.png' style="padding-left: 3px;position: fixed;bottom: 5px;"></div>
+
+</div>
+
+
+<script src="/scripts.js"></script>
+
+<script type="text/javascript" src="//downloads.mailchimp.com/js/signup-forms/popup/unique-methods/embed.js" data-dojo-config="usePlainJson: true, isDebug: false"></script><script type="text/javascript">window.dojoRequire(["mojo/signup-forms/Loader"], function(L) { L.start({"baseUrl":"mc.us20.list-manage.com","uuid":"0baba35be8f6397f7ac1066f1","lid":"5fd11d4221","uniqueMethods":true}) })</script>
+
 </html>
