6310608169f30ea329a44f245d172c75b5d849c3
NicolasRonsmans <ronsmans.nicolas@gmail.com>
2019-08-07 10:12:29 +0200

    bug: fix register password mismatch and default redirect

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -69,10 +69,10 @@
                         </div>
 
                         <div class="form-group row">
-                            {{ Form::label('password-confirm', __('Confirm Password'), [ 'class' => 'col-md-4 col-form-label text-md-right' ]) }}
+                            {{ Form::label('password_confirmation', __('Confirm Password'), [ 'class' => 'col-md-4 col-form-label text-md-right' ]) }}
 
                             <div class="col-md-6">
-                                {{ Form::password('password-confirm', [
+                                {{ Form::password('password_confirmation', [
                                     'class' => 'form-control' . ($errors->has('password_confirmation') ? ' is-invalid' : ''),
                                     'required' => true,
                                 ]) }}
