f9cf81e70a06d2c18e8c663cf15bd88c7cb25891
Alexander Patrick <adp90@case.edu>
2019-08-19 10:30:04 -0700

    Modify tests to check all locales.

--- tests/Feature/TweetRegexTest.php
+++ tests/Feature/TweetRegexTest.php
@@ -2,7 +2,7 @@
 
 namespace Tests\Feature;
 
-use Mcamara\LaravelLocalization\LaravelLocalization;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\Http\Controllers\TweetController;
@@ -19,33 +19,36 @@ class TweetRegexTest extends TestCase
      */
     public function testGenerateMessage()
     {
-        \App::setLocale('en');
-        $controller = new TweetController();
-        $service = new TweetRegexService();
-        $response = $controller->tweet($service);
+        foreach (LaravelLocalization::getSupportedLocales() as $locale => $value) {
+            \App::setLocale($locale);
+            $controller = new TweetController();
+            $service = new TweetRegexService();
+            $response = $controller->tweet($service);
 
-        // Need to make sure string is returned but string always varies.
-        // The challenge22 URL should always be there though.
-        $this->assertContains('challenge22', $response);
+            // Need to make sure string is returned but string always varies.
+            // The challenge22 URL should always be there though.
+            $this->assertContains('challenge22', $response);
+        }
     }
 
     /**
-     * Tests all English tweet resource files are valid.
+     * Tests that all tweet resource files are valid.
      *
      * @return void
      */
-    public function testEnglishTweetResources()
+    public function testTweetResources()
     {
-        \App::setLocale('en');
-        $stringArrays = __('tweet.tweet');
         $grammar  = new Read('hoa://Library/Regex/Grammar.pp');
         $compiler = Llk::load($grammar);
-        foreach ($stringArrays as $stringArray) {
-            foreach ($stringArray as $regex) {
-                $compiler->parse($regex);
+        foreach (LaravelLocalization::getSupportedLocales()  as $locale => $value) {
+            \App::setLocale($locale);
+            $stringArrays = __('tweet.tweet');
+            foreach ($stringArrays as $stringArray) {
+                foreach ($stringArray as $regex) {
+                    $compiler->parse($regex);
+                }
             }
         }
-        // Assert no exceptions thrown.
         $this->addToAssertionCount(1);
     }
 }
