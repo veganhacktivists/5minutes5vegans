7a5b4fb1bdbc0b0a2936541d649b7c492a8ec08d
Pablo Kebees <pablopetje@gmail.com>
2019-08-25 20:59:47 +0200

    Correct mail info

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'noreply@5minutes5vegans.org'),
+        'name' => env('MAIL_FROM_NAME', '5 Minutes 5 Vegans'),
     ],
 
     /*
