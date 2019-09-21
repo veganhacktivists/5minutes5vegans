33347b986ba0db5f59c541c199329cf53260b984
vagrant <vagrant@homestead>
2019-09-21 12:27:37 +0000

    included twitter

---
+++ resources/views/inc/twitter.blade.php
@@ -0,0 +1,5 @@
+<a class="twitter-timeline" data-lang="{{ App::getLocale() }}" data-theme="dark" data-link-color="#19CF86" href="https://twitter.com/vegassist?ref_src=twsrc%5Etfw">
+    Loading Twitter feed...<br/>
+    If this message doesn't go away, make sure you have scripts enabled and try refreshing the page.<br/>
+</a>
+<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
