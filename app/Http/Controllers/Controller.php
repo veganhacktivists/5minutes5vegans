6364f379e131b9dca263e0ef85a0d10d25467e08
Pablo Kebees <pablopetje@gmail.com>
2019-10-07 21:59:30 +0200

    Landing page redirect for authed users

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,10 +10,4 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-
-    public function index() {
-
-        return view('landing');
-
-    }
 }
