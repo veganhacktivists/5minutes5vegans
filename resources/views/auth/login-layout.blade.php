a4f81b3ce7b1b41a8d1e8026017f5410837a713f
Christopher <kurekc@hotmail.com>
2019-10-09 21:22:00 +0100

    Stuck footer to bottom, and added gradient to login overlay.

--- resources/views/auth/login-layout.blade.php
+++ resources/views/auth/login-layout.blade.php
@@ -11,82 +11,81 @@
 @section('page_content')
 <section id="login-bg" style="background-image: url({{ asset('images/login_bg.png') }});">
 </section>
-<section id="login-bg-overlay" style="background-image: url({{ asset("images/login_bg_overlay.png") }});">
+<section id="login-bg-overlay" style="background-image: url({{ asset("images/banner-activism.png") }});">
 </section>
-<div class="container-fluid">
-        <div class="container py-5">
-            <!-- Header -->
-            <div class="row">
-                <div class="col-sm">
-                    <a href="{{ route('feed') }}"><img class="w-25" src="{{ asset('images/logo.svg') }}"/></a>
+<div class="container-fluid login-container">
+    <div class="container py-5">
+        <!-- Header -->
+        <div class="row">
+            <div class="col-sm">
+                <a href="{{ route('feed') }}"><img class="w-25" src="{{ asset('images/logo.svg') }}"/></a>
+            </div>
+            <div class="col-sm text-right">
+                <a class="text-black btn font-bold btn-sm bg-transparent py-2 px-3 swirvy-box"
+                    style="border-color: #4dc0b5;"
+                    href="/">
+                    How does it work?
+                </a>
+            </div>
+        </div>
+        <!-- Content -->
+        <div class="row mt-5 mx-auto login-panes">
+            <div class="col login-pane-left">
+                @if (Route::getCurrentRoute()->getName() == 'login')
+                    <div id="login-pane">
+                        @yield('login-pane')
+                    </div>
+                @else
+                    <div id="register-pane">
+                        @yield('register-pane')
+                    </div>
+                @endif
+            </div>
+            <div class="col login-pane-right">
+                <h3>Start without account</h3>
+                <h5 class="my-5 font-weight-bold"><div class="horz-line"></div>PICK YOUR LANGUAGE.</h5>
+                <div class="d-flex mb-5 lang-flags">
+                    @foreach(LaravelLocalization::getSupportedLocales() as $localeCode => $properties)
+                        @php
+                            $active = (LaravelLocalization::getCurrentLocale() == $localeCode) ? true : false;
+                            $flag = strtolower(getCountry( $properties ));
+                        @endphp
+                            <a hreflang="{{ $localeCode }}"
+                                href="{{ LaravelLocalization::getLocalizedURL($localeCode, null, [], true) }}">
+                                <button type="button" class="btn {{ $active ? "login-flag-active" : "" }}">
+                                    <img src="{{ asset('images/landing/'.$flag).'.png' }}">
+                                </button>
+                            </a>
+                    @endforeach
                 </div>
-                <div class="col-sm text-right">
-                    <a class="text-black btn font-bold btn-sm bg-transparent py-2 px-3 swirvy-box"
-                       style="border-color: #4dc0b5;"
-                       href="/">
-                        How does it work?
+                <h5 class="mt-5 font-weight-bold"><div class="horz-line"></div>GET READY...</h5>
+                <div class="d-flex align-middle">
+                    <span class="mr-auto my-5 font-weight-bold text-white">And take up the challenge!</span>
+                    <a href="{{ route('feed') }}">
+                        <button id="play-submit-btn" class="btn start-btn px-0"><div><div><i class="fas fa-caret-right"></i></div></div></button>
                     </a>
                 </div>
             </div>
-            <!-- Content -->
-            <div class="row mt-5 mx-auto login-panes">
-                <div class="col login-pane-left">
-                    @if (Route::getCurrentRoute()->getName() == 'login')
-                        <div id="login-pane">
-                            @yield('login-pane')
-                        </div>
-                    @else
-                        <div id="register-pane">
-                            @yield('register-pane')
-                        </div>
-                    @endif
+        </div>
+    </div>
+</div>
+<!-- Footer -->
+<footer class="py-2 page-footer text-white">
+    <div class="container-fluid container-fluid-max">
+        <div class="row">
+            <div class="col-12 col-md-6 col-lg-4">
+                Follow us on <i class="fab fa-instagram"></i> {{ link_to('https://www.instagram.com/veganhacktivists', 'Instagram') }}
+            </div>
+            <div class="col-12 col-md-6 col-lg-4 text-center">
+                <div>
+                    Website hosted and designed by {{ link_to('https://veganhacktivists.org/', 'Vegan Hacktivists') }} with love
+                    <i class="far fa-heart"></i>
                 </div>
-                <div class="col login-pane-right">
-                    <h3>Start without account</h3>
-                    <h5 class="my-5 font-weight-bold"><div class="horz-line"></div>PICK YOUR LANGUAGE.</h5>
-                        <div class="d-flex mb-5 lang-flags">
-                            @foreach(LaravelLocalization::getSupportedLocales() as $localeCode => $properties)
-                                @php
-                                    $active = (LaravelLocalization::getCurrentLocale() == $localeCode) ? true : false;
-                                    $flag = strtolower(getCountry( $properties ));
-                                @endphp
-                                        <a hreflang="{{ $localeCode }}"
-                                          href="{{ LaravelLocalization::getLocalizedURL($localeCode, null, [], true) }}">
-                                            <button type="button" class="btn {{ $active ? "login-flag-active" : "" }}">
-                                                <img src="{{ asset('images/landing/'.$flag).'.png' }}">
-                                            </button>
-                                        </a>
-                            @endforeach
-                        </div>
-                        <h5 class="mt-5 font-weight-bold"><div class="horz-line"></div>GET READY...</h5>
-                        <div class="d-flex align-middle">
-                            <span class="mr-auto my-5 font-weight-bold text-white">And take up the challenge!</span>
-                            <a href="{{ route('feed') }}">
-                                <button id="play-submit-btn" class="btn start-btn px-0"><div><div><i class="fas fa-caret-right"></i></div></div></button>
-                            </a>
-                        </div>
-                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-4 text-right">
+                {{ link_to('https://www.patreon.com/veganhacktivists', 'Support us on Patreon') }}
             </div>
         </div>
-        <!-- Footer -->
-
     </div>
-    <footer class="py-2 page-footer text-white">
-            <div class="container-fluid container-fluid-max">
-                <div class="row">
-                <div class="col-12 col-md-6 col-lg-4">
-                    Follow us on <i class="fab fa-instagram"></i> {{ link_to('https://www.instagram.com/veganhacktivists', 'Instagram') }}
-                </div>
-                <div class="col-12 col-md-6 col-lg-4 text-center">
-                    <div>
-                    Website hosted and designed by {{ link_to('https://veganhacktivists.org/', 'Vegan Hacktivists') }} with love
-                        <i class="far fa-heart"></i>
-                    </div>
-                </div>
-                <div class="col-12 col-md-6 col-lg-4 text-right">
-                    {{ link_to('https://www.patreon.com/veganhacktivists', 'Support us on Patreon') }}
-                </div>
-                </div>
-            </div>
-        </footer>
+</footer>
 @endsection
