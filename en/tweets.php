0c0db7d36064c3136305bcac74bbf45f0edf451f
Sascha Laabs <tumbleweedxl@outlook.com>
2019-10-13 22:58:55 +0200

    add german translation
    * complete english translations
    * add german translation string

--- en/tweets.php
+++ en/tweets.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-
+    'loading' => 'Loading Twitter feed...<br/> If this message doesn\'t go away, make sure you have scripts enabled and try refreshing the page.<br/>',
     [
         'icon' => 'fas fa-info-circle',
         'title' => 'General info',
