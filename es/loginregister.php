dcebcdcbf610c25ac08476c6ff2d875fb295691e
Pablo Kebees <pablopetje@gmail.com>
2019-10-16 17:32:24 +0200

    Es: Login-register merge

---
+++ es/loginregister.php
@@ -0,0 +1,13 @@
+
+<?php
+return [
+    'how' => "¿Cómo funciona?",
+    'intro' => "Cuando registras una cuenta podrás editar y añadir respuestas rápidas de copiar para las preguntas más comunes que tienen la gente curiosa sobre el veganismo.",
+    'robot' => "Confirma que no eres un robot.",
+    'newsletter' => "Me gustaría recibir el boletín informativo",
+    'user' => "No encontramos ningún usuario con ese correo.",
+    'or-login' => "O iniciar sesión",
+    'or-register' => "O regístrate",
+    'login' => "Iniciar sesión",
+    'without-account' => "...¡o empieza sin una cuenta!"
+];
