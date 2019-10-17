a895bc659623b59dc40367586fcba600dd8addc1
moshpirit <7888062+moshpirit@users.noreply.github.com>
2019-10-17 13:46:51 +0200

    Create twitter.php

---
+++ es/twitter.php
@@ -0,0 +1,5 @@
+
+<?php
+return [
+    'loading' => 'Cargando el contenido de Twitter...<br/> Si este mensaje no desaparece, asegúrate de que tienes habilitados los scripts y refresca la página.<br/>',
+];
