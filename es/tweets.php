0ea2c0783a574f4f7b63e708acd13b4c8051e52e
moshpirit <7888062+moshpirit@users.noreply.github.com>
2019-12-02 02:47:19 +0100

    Tweets in Spanish
    I'd like to test them first before you make any announce to make sure they are ok

---
+++ es/tweets.php
@@ -0,0 +1,358 @@
+<?php
+
+return [
+    'loading' => 'Cargando el contenido de Twitter...<br/> Si este mensaje no desaparece, asegúrate de que tienes los scripts activados y prueba a refrescar la página.<br/>',
+    [
+        'icon' => 'fas fa-info-circle',
+        'title' => 'Información general',
+        'body' =>[
+            [
+                '¡Bien hecho! La mayoría de los veganos lamentamos no haber empezado antes\.'
+            ], [
+                'Aquí tienes algunos recursos: ' . preg_quote('http://reddit.com/r/Vegan') . ' (comunidad) ' . preg_quote('https://youtu.be/ny6aqdFy9SI?t=41') . ' (Documental) ' . preg_quote('https://www.youtube.com/playlist?list=PL03LZR09P2gQJyBgHk_XE8gbj8j9uFs8G') . ' (Mitos) 🐷 Envíame un MP si necesitas ayuda!'
+            ]
+        ]
+    ],
+
+    [
+        'icon' => 'fas fa-leaf',
+        'title' => 'Desafío 22+',
+        'body' =>[
+            [
+                'Hey!',
+                '¡Hola!',
+                '¡Qué bien leerlo!'
+            ], [
+                'Puedes hacerlo (definitivamente|absolutamente)? hazlo!',
+                'Aquí estamos (siempre)? aquí para ayudar!',
+                'Es (definitivamente|absolutamente)? muchísimo más fácil de lo que puedas pensar!'
+            ], [
+                'Deberías probar el Desafío 22\+!',
+                'El Desafío 22\+ puede ser justo lo que estás buscando\.',
+                'Hay un proyecto llamado Desafío 22\+ (muy interesante|que podrías probar)?\.'
+            ], [
+                'Te dan (un|tu propio) mentor (personal)? voluntario (en Facebook)? para ayudarte a ser vegan@ durante un mes\.',
+                'Tienes acceso a un mentor (personal)? (online|en Facebook)? para ayudarte a ser vegan@ durante un mes\.',
+                'Pueden ayudarte a ser vegan@ durante un mes con un mentor personal voluntario (por Facebook)?\.',
+                'Contactan contigo con un mentor (personal)? (en Facebook)? para ayudarte a ser vegan@ durante un mes\.',
+                'Puedes tener un mentor (personal)? voluntario (por Facebook)? para ayudarte a ser vegan@ durante un mes\.',
+                'Pueden contactarte (por Facebook)? con un mentor (personal)? voluntario para ayudarte a ser vegan@ durante un mes\.'
+            ], [
+                'El link es ' . preg_quote('https://www.challenge22.com/es') . ';',
+                'La URL es ' . preg_quote('https://www.challenge22.com/es') . ';',
+                'El enlace es ' . preg_quote('https://www.challenge22.com/es') . ';',
+                'La web es ' . preg_quote('https://www.challenge22.com/es') . ';',
+            ], [
+                '¡Buena suerte, puedes con ello!',
+                'Tú puedes con ello (perfectamente)?!',
+                'Estamos (todos)? contigo!',
+//                 'Hang in there!',
+                'Mantente fuerte!',
+                '¡Vamos! (Tú)? puedes con ello (perfectamente)?!',
+                'Sigue luchándolo!',
+                '(No|Nunca) te rindas!',
+                '¡A darlo todo!',
+                'Confía en ti!'
+            ], [
+                '¡Contáctame si necesitas (más)? ayuda!',
+                '¡Respóndeme si necesitas (más)? ayuda!',
+                '(Eres libre de|Siempre puedes) contactarme si necesitas (más)? ayuda!',
+                'Si necesitas (más)? ayuda dímelo!',
+                'Si (alguna vez)? necesitas (más)? ayuda mándame un (mensaje|MP)!'
+            ]
+        ]
+    ],
+
+    [
+        'icon' => 'fas fa-bacon',
+        'title' => 'Amo la carne',
+        'body' =>[
+            [
+                'Cada vez hay más carnes veganas desde estos últimos años!'
+            ], [
+                'Beyond Meat es excelente: ' . preg_quote('https://www.beyondmeat.com/') . ', y Heura también tienen muchos productos: ' . preg_quote('https://tinyurl.com/HeuraMapa') . '  Echa un vistazo en tu supermercado para ver qué productos hay disponibles, podrías sorprenderte!'
+            ]
+        ]
+    ],
+
+    [
+        'icon' => 'fas fa-cheese',
+        'title' => 'Amo el queso',
+        'body' =>[
+            [
+                'Los quesos veganos han evolucionado mucho estos últimos años, ¿los has probado?'
+            ], [
+                'Mi favorito es Violife, que tienen hasta para fundir, pero hay muchos más!'
+            ], [
+                'Si no, puedes comprarlos en ' . preg_quote('https://www.veggieroom.es/es/tipo/quesos-y-natas-vegetales') . '  o en ' . preg_quote('https://www.vegaffinity.com/tienda-vegana/44-comprar-quesos-veganos') . '¡Si tienes alguna duda aquí estoy! 😊'
+            ]
+        ]
+    ],
+
+    [
+        'icon' => 'fas fa-pizza-slice',
+        'title' => 'Amo la Pizza',
+        'body' =>[
+            [
+                'Puedes encontrar productos veganos como pizzas (y restaurantes) en ' . preg_quote('http://HappyCow.net') . '!'
+            ], [
+                'Hay muchas marcas de pizzas veganas, como las de Mercadona o Lidl: ' . preg_quote('https://ideavegana.com/pizza-vegana-mercadona/') . ' por supuesto, siempre están las caseras. Si quieres más info dímelo y te ayudo\.'
+            ]
+        ]
+    ],
+
+    [
+        'icon' => 'fas fa-egg',
+        'title' => 'Amo el huevo',
+        'body' =>[
+            [
+                'Hay muy buenas recetas con huevo veganizadas (como esta, pero con espinaca): ' . preg_quote('https://danzadefogones.com/revuelto-vegano-tofu/')
+            ], [
+                '🐣 También puedes encontrar sustitutivos en
+: ' . preg_quote('https://soycomocomo.es/especialista/biogra/huevo-vegano') . ' o comprarlos directamente como los “huevos” veganos No Egg (de Orgran)\.'
+            ]
+        ]
+    ],
+
+    [
+        'icon' => 'fas fa-ice-cream',
+        'title' => 'Amo el helado',
+        'body' =>[
+            [
+                'Prueba los helados veganos de Ben&Jerry\'s: ' . preg_quote('https://www.ben-jerrys.es/sabores/vegano')
+            ], [
+                'Tienen muchos sabores y también hay otras marcas con helados de soja/almendras/coco/etc!'
+            ], [
+                '(Mira:|Échale un ojo a) ' . preg_quote('https://ideavegana.com/?s=helado 
+')
+            ]
+        ]
+    ],
+
+    [
+        'icon' => 'fas fa-drumstick-bite',
+        'title' => 'Amo el pollo',
+        'body' =>[
+            [
+                'Heura tiene un pollo increíble: ' . preg_quote('https://tinyurl.com/HeuraMapa'') . '
+            ], [
+                '🐣 HappyCow para comer fuera: ' . preg_quote('https://www.happycow.net') . '! échales un ojo, seguro que encuentras sitios interesantes cerca que pueden sorprenderte! 😉 Si tienes cualquier duda dímelo! 😊 '
+            ]
+        ]
+    ],
+
+    [
+        'icon' => 'fas fa-glass-whiskey',
+        'title' => 'Leches vegetales',
+        'body' =>[
+            [
+                'Mira este vídeo de sólo 5 min. (puedes activar subtítulos) ' . preg_quote('https://youtu.be/UcN7SGGoCNI')
+            ], [
+                'Existen muchas leches vegetales deliciosas de: soja, almendras, avena, coco, arroz, etc!'
+            ], [
+                'No se ponen malas, son más saludables y no necesitan refrigeración antes de abrirlas.'
+            ]
+        ]
+    ],
+
+    [
+        'icon' => 'fas fa-video',
+        'title' => 'Documentales',
+        'body' =>[
+            [
+                'Aquí tienes un poco de motivación! Cowspiracy (Medio ambiente): ' . preg_quote('https://tinyurl.com/cownetflix') . ' The Game Changers (Salud): ' . preg_quote('https://tinyurl.com/changersnetflix') . ' Dominion (Animales): ' . preg_quote('https://youtu.be/ny6aqdFy9SI?t=40')
+            ], [
+                'Avísame si ves alguno!! 🌱'
+            ]
+        ]
+    ],
+
+    [
+        'icon' => 'fas fa-sad-cry',
+        'title' => 'Película Dominion',
+        'body' =>[
+            [
+                'Te recomiendo que veas este vídeo de YouTube: ' . preg_quote('https://youtu.be/J_G3ZhimsMw?t=40')
+            ], [
+                'Si tienes tiempo, los últimos 15 minutos te pueden cambiar la vida.'
+            ], [
+                'Mándame un mensaje si quieres ayuda para hacerte vegano y te responderé lo que quieras gustosamente!'
+            ]
+        ]
+    ],
+
+    [
+        'icon' => 'fas fa-baby',
+        'title' => 'Poco a poco',
+        'body' =>[
+            [
+                'Es fantástico que tengas interés en el veganismo! 🐮'
+            ], [
+                'Si crees que es muy difícil y no sabes cómo hacerlo, prueba a ir poco a poco\.'
+            ], [
+                'Empieza por probar y acostumbrarte a beber leches vegetales.'
+            ], [
+                'Luego mantequilla, carnes, etc\. Más info (activa subtítulos): ' . preg_quote('https://www.youtube.com/playlist?list=PL03LZR09P2gQJyBgHk_XE8gbj8j9uFs8G')
+            ]
+        ]
+    ],
+
+    [
+        'icon' => 'fas fa-leaf',
+        'title' => 'Vegetariano/a',
+        'body' =>[
+            [
+                'El vegetarianismo es genial 😊'
+            ], [
+                'pero te recomendaría ver este vídeo (con subtítulos): ' . preg_quote('https://www.youtube.com/watch?v=uWna6-niYEg')
+            ], [
+                'Algunos argumentos desmentidos (con subs): ' . preg_quote('https://www.youtube.com/playlist?list=PL03LZR09P2gQJyBgHk_XE8gbj8j9uFs8G') . ' y el desafío de 1 mes de veganismo: ' . preg_quote('https://www.challenge22.com/es')
+            ]
+        ]
+    ],
+
+    [
+        'icon' => 'fas fa-coins',
+        'title' => 'Es muy caro',
+        'body' =>[
+            [
+                'En realidad, ser vegan es muy barato! Sólo es caro si compras muchos productos “especiales”'
+            ], [
+                'Patatas, arroz, tofu, legumbres, verdura, fruta, pasta, avena, son todas muy baratas!         
+   ], [
+                'Gran vídeo (con subtítulos) sobre ello: ' . preg_quote('https://www.youtube.com/watch?v=Vs_nXVmyP1E')
+            ]
+        ]
+    ],
+
+    [
+        'icon' => 'fas fa-calculator',
+        'title' => 'Las proteínas',
+        'body' =>[
+            [
+                'Todo tipo de comida: verduras, frutas, semillas, etc tiene proteínas, de las que no necesitamos tantas\.'
+            ], [
+                'Este enlace trata el tema: ' . preg_quote('https://vegetarianismo.net/nutricion/proteinas.htm') . ' más en: ' . preg_quote('https://yourveganfallacyis.com/es/veganos-no-pueden-obtener-suficiente-proteina/fuentes'). ' y ' . preg_quote('https://www.vivahealth.org.uk/resources/protein-myth-fact-sheet').  ' si tienes dudas o quieres más infornación aquí estoy 😊'
+            ]
+        ]
+    ],
+
+    [
+        'icon' => 'fas fa-dumbbell',
+        'title' => 'Dieta deportiva',
+        'body' =>[
+            [
+                'El hombre más fuerte del mundo es vegano, y hay muchos otros récords de deportistas veganos'
+            ], [
+                'Una buena dieta vegetal es la mejor para tu entrenamiento!'
+            ], [
+                'Vídeos: ' . preg_quote('https://www.youtube.com/results?search_query=vegan+fitness') . ' y comunidad (en inglés): ' . preg_quote('https://www.reddit.com/r/veganfitness/')
+            ]
+        ]
+    ],
+
+    [
+        'icon' => 'fas fa-comments',
+        'title' => 'Comunidad',
+        'body' =>[
+            [
+                'Si quieres hablar con gente o simplemente ayuda, échale un ojo a: ' . preg_quote('http://reddit.com/r/Vegan') . '!'
+            ], [
+                'Es una comunidad de más de 300,000 veganos con experiencia, pregúntales lo que quieras\.'
+            ], [
+                'También existen /r/VeganFitness, /r/VeganRecipes, y muchos más!'
+            ]
+        ]
+    ],
+
+    [
+        'icon' => 'fas fa-utensils',
+        'title' => 'Comer fuera',
+        'body' =>[
+            [
+                'Comer fuera puede ser difícil si no sabes dónde ir o qué pedir, pero se hace mucho más fácil cuando lo hayas hecho varias veces\.'
+            ], [
+                'Hasta entonces, te puedes ayudar de: ' . preg_quote('http://happycow.net') . ' para encontrar opciones veganas! 🐮'
+            ]
+        ]
+    ],
+
+    [
+        'icon' => 'fas fa-tree',
+        'title' => 'Medio ambiente',
+        'body' =>[
+            [
+                'Hay estadísticas basadas en artículos y estudios realmente agobiantes sobre lo que las industrias cárnica y láctea están haciéndole al medio ambiente: ' . preg_quote('https://youaretheirvoice.com/pages/the-daunting-facts') . ' Ej: la ganadería es responsable de hasta un 91% de la destrucción del Amazonas\.'
+            ]
+        ]
+    ],
+
+    [
+        'icon' => 'fas fa-flask',
+        'title' => 'Consenso científico',
+        'body' =>[
+            [
+                'Hoy en día existe consenso en que el veganismo es más sano que una dieta con carnes\.'
+            ], [
+                'Esta página muestra las fuentes directamente de las organizaciones más importantes de salud, dietética y nutrición (que son objetivas):' . preg_quote('https://youaretheirvoice.com/pages/the-clear-consensus')
+            ]
+        ]
+    ],
+
+    [
+        'icon' => 'fas fa-heartbeat',
+        'title' => 'Salud',
+        'body' =>[
+            [
+                'Hoy en día se sabe que una dieta vegana es mucho más saludable que una dieta tradicional\.'
+            ], [
+                'Las carnes rojas y procesadas causan enfermedades como: cáncer, problemas de corazón, diabetes y obesidad\.'
+            ], [
+                'Esta web tiene info y estudios: ' . preg_quote('https://youaretheirvoice.com/pages/the-clear-consensus')
+            ]
+        ]
+    ],
+
+    [
+        'icon' => 'fas fa-leaf',
+        'title' => 'Qué pereza cocinar',
+        'body' =>[
+            [
+                'Muchos veganos, en vez de cocinar, compran procesados veganos o comen en restaurantes\.'
+            ], [
+                'No es necesario aprender a cocinar para tener una dieta saludable vegana\.'
+            ], [
+                'Comer fuera es muy fácil con ' . preg_quote('http://happycow.net') . ' 🐮 MP si quieres ayuda!'
+            ]
+        ]
+    ],
+
+    [
+        'icon' => 'fas fa-meh-blank',
+        'title' => 'La comida vegana aburre',
+        'body' =>[
+            [
+                'Se puede hacer una versión vegana de casi toda la comida que conoces, sólo tienes que saber cocinarla o dónde comprarla\.'
+            ], [
+                'Algunas recetas: ' . preg_quote('https://veganuary.com/es/recipes') . ' (o busca tu plato favorito + "Vegan").'
+            ], [
+                'Para comer fuera: ' . preg_quote('https://happycow.net')
+            ]
+        ]
+    ],
+
+    [
+        'icon' => 'fas fa-fist-raised',
+        'title' => '30+ razones',
+        'body' =>[
+            [
+                '¡Ese es un mito muy común! 😅 Mira: ' . preg_quote('https://yourveganfallacyis.com/es') . '; básicamente desmiente los mitos más comunes sobre el veganismo\,'
+            ], [
+                'En vídeos (con subtítulos) para los 30 mitos más comunes: ' . preg_quote('https://earthlinged.org/30excuses/')
+            ], [
+                'Mándame un MP si necesitas ayuda o tienes preguntas!'
+            ]
+        ]
+    ],
+
+];
