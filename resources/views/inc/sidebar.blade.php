d401e7a659f0fad428d3e5b2c3f67740684d6bd6
Pablo Kebees <pablopetje@gmail.com>
2019-12-04 21:27:06 +0100

    Fix login/register link localisation

--- resources/views/inc/sidebar.blade.php
+++ resources/views/inc/sidebar.blade.php
@@ -6,15 +6,15 @@
 </div>
 
 <div class="d-flex justify-content-between">
-    <h3 id="easy-to-copy-resources" class="pb-1">EASY TO COPY RESOURCES</h3>
+    <h3 id="easy-to-copy-resources" class="pb-1">@lang('EASY TO COPY RESOURCES')</h3>
     <div id="auth-links-inline">
         @auth
-            {{ link_to_route('logout', __('Logout'), null, ['class' => 'auth-link', 'onclick' => 'event.preventDefault(); $("#logout-form").submit();']) }}
+            {{ link_to_route('logout', __('logout'), null, ['class' => 'auth-link', 'onclick' => 'event.preventDefault(); $("#logout-form").submit();']) }}
         @endauth
         @guest
-            {{ link_to_route('register', __('register'), null, array('class' => 'auth-link')) }}
-            or
-            {{ link_to_route('login', __('login'), null, array('class' => 'auth-link')) }}
+            {{ link_to_route('register', __('Register'), null, array('class' => 'auth-link')) }}
+            @lang('or')
+            {{ link_to_route('login', __('Login'), null, array('class' => 'auth-link')) }}
         @endguest
     </div>
 </div>
