bdd2f7a35a13fe263b29db510cd9b4d27e63057e
David van Beveren <admin@veganhacktivists.org>
2020-08-02 17:06:57 +0200

    Increase font size for copy textbox

--- resources/views/verbiage/index.blade.php
+++ resources/views/verbiage/index.blade.php
@@ -2,7 +2,7 @@
     @each( 'verbiage.verbiage', $verbiages, 'verbiage' )
 </div>
 <div class="p-2 row">
-    <textarea class="col w-100 verbiage-message p-3" rows="4"></textarea>
+    <textarea class="col w-100 verbiage-message p-3" rows="4" style="font-size: 18px;"></textarea>
     <div class="col-auto d-flex flex-column justify-content-between">
         <button class="btn btn-primary">@lang('New')</button>
         <button class="btn btn-success invisible">@lang('Save')</button>
