e1083437421fe2a38c915315e23590126d07c5c2
David van Beveren <admin@veganhacktivists.org>
2020-08-02 18:07:00 +0200

    Update

--- resources/js/components/Verbiages.vue
+++ resources/js/components/Verbiages.vue
@@ -75,10 +75,12 @@
                     v-model="selected.body"
                     class="w-100 p-3"
                     rows="4"
+					style="font-size: 18px;"
                     v-bind:disabled="busy"
                     v-on:keyup="characterCountdown"
                     :placeholder="[[defaultMessage]]"
                 ></textarea>
+				<p style="font-color:#1c522apx;">Want to write and copy your own resources? <a href="" style="font-size: 16px;"><u>Register</u></a> or <a href="" style="font-size: 16px;"><u>Login</u></a>!</p>
                 <button
                     data-toggle="tooltip"
                     class="btn btn-link copy-btn"
--- resources/views/verbiage/index.blade.php
+++ resources/views/verbiage/index.blade.php
@@ -2,8 +2,7 @@
     @each( 'verbiage.verbiage', $verbiages, 'verbiage' )
 </div>
 <div class="p-2 row">
-    <textarea class="col w-100 verbiage-message p-3" rows="4" style="font-size: 18px;"></textarea>
-	<p style="font-color:#1c522apx;">Want to write and copy your own resources? <a href="" style="font-size: 16px;"><u>Register</u></a> or <a href="" style="font-size: 16px;"><u>Login</u></a>!</p>
+    <textarea class="col w-100 verbiage-message p-3" rows="4"></textarea>
     <div class="col-auto d-flex flex-column justify-content-between">
         <button class="btn btn-primary">@lang('New')</button>
         <button class="btn btn-success invisible">@lang('Save')</button>
