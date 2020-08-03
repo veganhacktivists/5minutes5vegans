e28b316692121b4dbf8bf8ecfa5d5b395ae08450
David van Beveren <admin@veganhacktivists.org>
2020-08-03 10:48:34 +0100

    Update index.blade.php

--- resources/views/verbiage/index.blade.php
+++ resources/views/verbiage/index.blade.php
@@ -4,8 +4,8 @@
 <div class="p-2 row">
     <textarea class="col w-100 verbiage-message p-3" rows="4"></textarea>
     <div class="col-auto d-flex flex-column justify-content-between">
-        <button class="btn btn-primary">@lang('New')</button>
-        <button class="btn btn-success invisible">@lang('Save')</button>
+        <button class="btn btn-primary" style="margin-bottom: 15px;margin-top: 15px;">@lang('New')</button>
+        <button class="btn btn-success invisible" style="margin-bottom: 15px;">@lang('Save')</button>
         <button class="btn btn-danger invisible">@lang('Delete')</button>
     </div>
 </div>
