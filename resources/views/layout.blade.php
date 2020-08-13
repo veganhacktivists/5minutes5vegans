83a92d9f9d9409d8329b07e89213dc67df2ace93
David van Beveren <admin@veganhacktivists.org>
2020-08-13 20:13:36 +0100

    google analytics

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,6 +1,16 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-150398169-3"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-150398169-3');
+</script>
+
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
