29a593584c5d210a9df3b98d44b04cd13afd6156
Headscracher <t.frohme@all4-one.de>
2024-04-16 09:55:16 +0200

    Update package.json

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,6 +1,5 @@
-window._ = require('lodash');
 window.$ = window.jQuery = require('jquery');
-window.Popper = require('@popperjs/core');
+window.Popper = require('popper.js');
 require('bootstrap');
 
 /**
