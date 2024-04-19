583fdcb1e5f83661b663acb4305974451855783a
Jérémy Touati <sielver@gmail.com>
2024-04-19 17:57:27 +0200

    Remove useless div
    (cherry picked from commit dc7478416bd598ae7a93d0ecdb38f11cfbeef538)

--- resources/views/inc/twitter.blade.php
+++ resources/views/inc/twitter.blade.php
@@ -1,10 +1,8 @@
 <div class="timeline">
     @foreach ($tweets as $tweet)
         <a class="tweet" href="https://twitter.com/_/status/{{ $tweet->id }}" target="_blank">
-            <div>
-                <p>{{ $tweet->from }}</p>
-                <p>{{ $tweet->text }}</p>
-            </div>
+            <p>{{ $tweet->from }}</p>
+            <p>{{ $tweet->text }}</p>
         </a>
     @endforeach
 </div>
