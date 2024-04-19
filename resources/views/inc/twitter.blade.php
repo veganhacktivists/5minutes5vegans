48d312a9c41cd88343b2b024a5520cc17e7eb7b6
Jérémy Touati <sielver@gmail.com>
2024-04-19 17:52:07 +0200

    Build basic feed UI
    (cherry picked from commit 2a1a03080ef3771991dc399c65ad9ce13a1396e2)

--- resources/views/inc/twitter.blade.php
+++ resources/views/inc/twitter.blade.php
@@ -1,29 +1,10 @@
-<div id="Twitter" class="iframecontent">
-    <a
-        href="https://twitter.com/5m5v_{{ App::getLocale() }}"
-        class="twitter-timeline"
-        data-lang="{{ App::getLocale() }}"
-        data-theme="light"
-        data-link-color="#19CF86">
-    </a>
-    <div class="twitter-timeline" id="timeline-loading">
-        <strong>@lang('twitter.loading')</strong>
-        <div class="mt-3">
-            <p>
-                <strong>@lang('twitter.note_label')</strong>
-                @lang('twitter.note_body')
-            </p>
-        </div>
-        <div class="mt-3">
-            @lang('twitter.try_again', [
-                'twitter_link' => "https://twitter.com/5m5v_{{ App::getLocale() }}"
-            ])
-        </div>
-    </div>
-    <script
-        async
-        src="https://platform.twitter.com/widgets.js"
-        charset="utf-8"
-        onload="document.getElementById('timeline-loading').remove()">
-    </script>
+<div class="timeline">
+    @foreach ($tweets as $tweet)
+        <a class="tweet" href="https://twitter.com/_/status/{{ $tweet->id }}" target="_blank">
+            <div>
+                <p>{{ $tweet->from }}</p>
+                <p>{{ $tweet->text }}</p>
+            </div>
+        </a>
+    @endforeach
 </div>
