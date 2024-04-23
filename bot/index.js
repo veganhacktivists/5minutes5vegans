4ad1e514052645be22a27d5d79d954e19e45946a
Jérémy Touati <sielver@gmail.com>
2024-04-23 17:58:29 +0200

    Fix endpoint

--- bot/index.js
+++ bot/index.js
@@ -33,7 +33,7 @@ while (true) {
             try {
                 logInfo(`Sending ${tweets.length} new tweets...`);
 
-                await axios.post(`${process.env.APP_URL}/tweets`, { lang: user.lang, tweets });
+                await axios.post(`${process.env.APP_URL}/api/tweets`, { lang: user.lang, tweets });
 
                 logInfo('Sent!');
 
