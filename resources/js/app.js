693b225518870dfd4f36c9e11258b9a59baa1187
Joaquín Triñanes <joaquin@veganhacktivists.org>
2024-06-21 22:12:25 +0200

    Format js files

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,50 +4,50 @@
  * application frontend using useful Laravel and JavaScript libraries.
  */
 
-require('./bootstrap')
-window.axios = require('axios')
-import Swiper from 'swiper/bundle'
-import Clipboard from 'v-clipboard'
-import { createApp } from 'vue'
-import App from './components/App.vue'
+require('./bootstrap');
+window.axios = require('axios');
+import Swiper from 'swiper/bundle';
+import Clipboard from 'v-clipboard';
+import { createApp } from 'vue';
+import App from './components/App.vue';
 
 /*********
  * TIMER *
  *********/
 
-var startTime, timerInterval
+var startTime, timerInterval;
 $(() => {
-    startTimer()
-    $('.timer-restart').click(startTimer)
-})
+    startTimer();
+    $('.timer-restart').click(startTimer);
+});
 
 function setTimer(minutes, seconds) {
-    $('.timer-display .minutes').html(minutes)
-    $('.timer-display .seconds').html(seconds)
+    $('.timer-display .minutes').html(minutes);
+    $('.timer-display .seconds').html(seconds);
 }
 
 function startTimer() {
-    startTime = Date.now()
-    timerInterval = setInterval(updateTimer, 1000)
-    setTimer('05', '00')
-    $('.timer-complete').hide(400)
-    $('.timer-section').show(400)
+    startTime = Date.now();
+    timerInterval = setInterval(updateTimer, 1000);
+    setTimer('05', '00');
+    $('.timer-complete').hide(400);
+    $('.timer-section').show(400);
 }
 
 function updateTimer() {
-    var timestamp = 5 * 60 * 1000 + startTime - Date.now()
+    var timestamp = 5 * 60 * 1000 + startTime - Date.now();
     if (timestamp < 0) {
-        clearInterval(timerInterval)
-        $('.timer-section').hide(400)
-        $('.timer-complete').show(400)
-        return
+        clearInterval(timerInterval);
+        $('.timer-section').hide(400);
+        $('.timer-complete').show(400);
+        return;
     }
 
-    var time = new Date(timestamp)
+    var time = new Date(timestamp);
     setTimer(
         ('0' + time.getMinutes()).substr(-2),
         ('0' + time.getSeconds()).substr(-2),
-    )
+    );
 }
 
 // Vue.use(Clipboard)
@@ -58,24 +58,24 @@ function updateTimer() {
 // Vue.component('UserEdit', require('./components/UserEdit.vue').default)
 
 // Register CSRF token for use with vue-resource
-let token = document.head.querySelector('meta[name="csrf-token"]')
+let token = document.head.querySelector('meta[name="csrf-token"]');
 
 if (token) {
     window.axios.defaults.headers.common = {
         'X-Requested-With': 'XMLHttpRequest',
         'X-CSRF-TOKEN': token.content,
-    }
+    };
 } else
     console.error(
         'CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token',
-    )
+    );
 
 $(() => {
-    initializeMdForm()
+    initializeMdForm();
     if ($('app').length) {
-        createApp(App).use(Clipboard).mount('app')
+        createApp(App).use(Clipboard).mount('app');
     }
-})
+});
 
 /******************
  * MD Form Helper *
@@ -83,11 +83,11 @@ $(() => {
 function initializeMdForm() {
     $('.form-control-md').blur((event) => {
         if ($(event.target).val()) {
-            $(event.target).addClass('has-value')
+            $(event.target).addClass('has-value');
         } else {
-            $(event.target).removeClass('has-value')
+            $(event.target).removeClass('has-value');
         }
-    })
+    });
 }
 
 /******************************
@@ -95,19 +95,19 @@ function initializeMdForm() {
  ******************************/
 $(() => {
     if (!$('.swiper-container').length) {
-        return
+        return;
     }
     window.mySwiper = new Swiper('.swiper-container', {
         direction: 'horizontal',
         pagination: {
             el: '.swiper-pagination',
             clickable: true,
-            renderBullet: function(index, className) {
+            renderBullet: function (index, className) {
                 return `
                 <span class="swirvy-box ${className}">${
                     index === 0 ? 'resources' : 'feed'
                 }</span>
-                `
+                `;
             },
         },
         navigation: {
@@ -130,36 +130,36 @@ $(() => {
         },
         watchOverflow: true,
         allowTouchMove: false,
-    })
+    });
 
     // Start on the twitter slide in mobile view
-    window.mySwiper.slideTo(2, false, false)
-})
+    window.mySwiper.slideTo(2, false, false);
+});
 
 // Scroll animation for landing page
-$(window).scroll(function() {
+$(window).scroll(function () {
     if ($(window).scrollTop() > 15) {
-        $('.landing header').addClass('header-scroll')
-        $('#landing-logo-mobile').addClass('logo-scroll')
+        $('.landing header').addClass('header-scroll');
+        $('#landing-logo-mobile').addClass('logo-scroll');
     } else {
-        $('.landing header').removeClass('header-scroll')
-        $('#landing-logo-mobile').removeClass('logo-scroll')
+        $('.landing header').removeClass('header-scroll');
+        $('#landing-logo-mobile').removeClass('logo-scroll');
     }
-})
+});
 
 /************************************
  * Script to reset the timer *
  ************************************/
-window.onload = function() {
+window.onload = function () {
     // set the focus to the language bar
-    document.getElementById('languageFlags').focus()
+    document.getElementById('languageFlags').focus();
 
     // reset the timer by clicking the icon
-    var as = document.querySelectorAll('#resetLink')
-    as.forEach(function(a) {
-        a.onclick = function() {
-            startTimer()
-            return false
-        }
-    })
-}
+    var as = document.querySelectorAll('#resetLink');
+    as.forEach(function (a) {
+        a.onclick = function () {
+            startTimer();
+            return false;
+        };
+    });
+};
--- resources/js/components/Menu.vue
+++ resources/js/components/Menu.vue
@@ -1,73 +1,88 @@
 <template>
-   <div class="verbiage-menu">
-      <div v-if="currentUser" style="margin: 10px;" class="d-flex">
-         <div class="flex-grow-1 d-flex align-items-center greeting">
-            <p class="m-0">
-            Welcome, {{ currentUser.name }}!
-            </p>
-         </div>
+    <div class="verbiage-menu">
+        <div v-if="currentUser" style="margin: 10px" class="d-flex">
+            <div class="flex-grow-1 d-flex align-items-center greeting">
+                <p class="m-0">Welcome, {{ currentUser.name }}!</p>
+            </div>
 
-         <a href="#"
-            role="button"
-            :class="{ 'active': !custom }"
-            @click.prevent="$emit('navigate', 'verbiages'), $emit('toggleVerbiage', false)"
-            class="verbiage-switch"
-            >Default</a>
-         <a href="#"
-            role="button"
-            :class="{ 'active': custom }"
-            @click.prevent="$emit('navigate', 'verbiages'), $emit('toggleVerbiage', true)"
-            class="verbiage-switch"
-            >Customized</a>
-         <a href="#"
-            @click.prevent="$emit('navigate', 'userEdit')"
-            role="button"
-            style="background-color: #BCDCF0"
-            >Edit Profile</a>
-         <a href="#"
-            onclick="event.preventDefault(); $('#logout-form').submit();"
-            role="button"
-            style="background-color: #E9D0D0"
-            >Logout</a>
-      </div>
+            <a
+                href="#"
+                role="button"
+                :class="{ active: !custom }"
+                @click.prevent="
+                    $emit('navigate', 'verbiages'),
+                        $emit('toggleVerbiage', false)
+                "
+                class="verbiage-switch"
+                >Default</a
+            >
+            <a
+                href="#"
+                role="button"
+                :class="{ active: custom }"
+                @click.prevent="
+                    $emit('navigate', 'verbiages'),
+                        $emit('toggleVerbiage', true)
+                "
+                class="verbiage-switch"
+                >Customized</a
+            >
+            <a
+                href="#"
+                @click.prevent="$emit('navigate', 'userEdit')"
+                role="button"
+                style="background-color: #bcdcf0"
+                >Edit Profile</a
+            >
+            <a
+                href="#"
+                onclick="event.preventDefault(); $('#logout-form').submit();"
+                role="button"
+                style="background-color: #e9d0d0"
+                >Logout</a
+            >
+        </div>
 
-      <div v-else style="margin: 10px;" class="d-flex">
-         <div class="flex-grow-1 d-flex align-items-center greeting">
-            <p class="mb-0">
-            Welcome, guest!
-            </p>
-         </div>
+        <div v-else style="margin: 10px" class="d-flex">
+            <div class="flex-grow-1 d-flex align-items-center greeting">
+                <p class="mb-0">Welcome, guest!</p>
+            </div>
 
-         <a href="#"
-            role="button"
-            class="verbiage-switch verbiage-active"
-            >Default</a>
-         <a :href="routes.login"
-            role="button"
-            class="verbiage-switch verbiage-inactive"
-            >Customized</a>
-         <a :href="routes.login"
-            role="button"
-            class="verbiage-switch"
-            style="background-color: #E9D0D0"
-            >Login</a>
-         <a :href="routes.register"
-            role="button"
-            style="background-color: #BCDCF0"
-            >Register</a>
-      </div>
-   </div>
+            <a href="#" role="button" class="verbiage-switch verbiage-active"
+                >Default</a
+            >
+            <a
+                :href="routes.login"
+                role="button"
+                class="verbiage-switch verbiage-inactive"
+                >Customized</a
+            >
+            <a
+                :href="routes.login"
+                role="button"
+                class="verbiage-switch"
+                style="background-color: #e9d0d0"
+                >Login</a
+            >
+            <a
+                :href="routes.register"
+                role="button"
+                style="background-color: #bcdcf0"
+                >Register</a
+            >
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-   props: ['custom'],
+    props: ['custom'],
 
-   data () {
-      return {
-         currentUser: window.currentUser,
-         routes: window.routes
-      }
-   },
-}
+    data() {
+        return {
+            currentUser: window.currentUser,
+            routes: window.routes,
+        };
+    },
+};
 </script>
