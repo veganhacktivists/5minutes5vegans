1412899cbe567795f7cf4667f31dc6014dc18495
Joaquín Triñanes <joaquin@veganhacktivists.org>
2024-06-21 22:12:25 +0200

    Format js files

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,32 +1,32 @@
 <template>
-   <div>
-      <Menu
-         @navigate="page = $event"
-         @toggleVerbiage="custom = $event"
-         :custom="custom"
-         />
+    <div>
+        <Menu
+            @navigate="page = $event"
+            @toggleVerbiage="custom = $event"
+            :custom="custom"
+        />
 
-      <Verbiages v-if="page === 'verbiages'" :custom="custom" />
-      <UserEdit v-if="page === 'userEdit'" />
-   </div>
+        <Verbiages v-if="page === 'verbiages'" :custom="custom" />
+        <UserEdit v-if="page === 'userEdit'" />
+    </div>
 </template>
 
 <script>
-import Menu from './Menu.vue'
-import Verbiages from './Verbiages.vue'
-import UserEdit from './UserEdit.vue'
+import Menu from './Menu.vue';
+import Verbiages from './Verbiages.vue';
+import UserEdit from './UserEdit.vue';
 export default {
-   components: {
-     Menu,
-     Verbiages,
-     UserEdit
-   },
-   data () {
-      return {
-         custom: false,
-         page: 'verbiages',
-         routes: window.routes,
-      }
-   }
-}
+    components: {
+        Menu,
+        Verbiages,
+        UserEdit,
+    },
+    data() {
+        return {
+            custom: false,
+            page: 'verbiages',
+            routes: window.routes,
+        };
+    },
+};
 </script>
