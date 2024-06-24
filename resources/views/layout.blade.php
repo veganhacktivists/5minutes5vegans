bec78536980308934dc7e41b2684962aeba9f8c0
Joaquín Triñanes <joaquin@veganhacktivists.org>
2024-06-24 17:29:59 +0200

    Remove duplicated google script
    laravel-cookie-consent takes care of inserting it

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,21 +2,6 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async
-        src="https://www.googletagmanager.com/gtag/js?id={{ env('GOOGLE_ANALYTICS_ID') }}">
-    </script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-
-        function gtag() {
-            dataLayer.push(arguments);
-        }
-        gtag('js', new Date());
-
-        gtag('config', '{{ env('GOOGLE_ANALYTICS_ID') }}');
-    </script>
-
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
