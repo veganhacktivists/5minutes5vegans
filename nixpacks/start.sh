a18d0856eb8b5291e0e1f4d7bbafb2bbb34fb953
Jérémy Touati <sielver@gmail.com>
2024-06-28 10:40:40 +0200

    Set up Nixpacks config

---
+++ nixpacks/start.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Run Laravel migrations
+php artisan migrate --force
+
+# Cache Laravel views
+php artisan view:cache
+
+# Cache Laravel config
+php artisan config:cache
+
+# Optimize Laravel
+php artisan optimize
+
+# Transform the nginx configuration
+node /assets/scripts/prestart.mjs ./nixpacks/nginx.template.conf /etc/nginx.conf
+
+# Start PHP-FPM
+php-fpm -y ./nixpacks/php-fpm.conf
+
+# Start Supervisor
+supervisord -c /etc/supervisord.conf
+
+# Start Nginx
+nginx -c /etc/nginx.conf
