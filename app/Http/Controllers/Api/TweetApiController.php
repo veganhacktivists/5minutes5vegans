bedfe500da7ec59d0af91099f15198f13e1aec24
Jérémy Touati <sielver@gmail.com>
2024-09-24 11:27:00 +0200

    CS

--- app/Http/Controllers/Api/TweetApiController.php
+++ app/Http/Controllers/Api/TweetApiController.php
@@ -3,33 +3,41 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use Lib\ValidationRules;
 use App\Models\Tweet;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Lib\ValidationRules;
 
 class TweetApiController extends Controller
 {
-
-    public function store(Request $request)
+    public function store(Request $request): JsonResponse
     {
         $validator = Validator::make($request->all(), [
-            'lang' => 'required|string|max:2|in:en,fr,es,de',
+            'lang'   => 'required|string|max:2|in:en,fr,es,de',
             'tweets' => 'required|array',
             ...ValidationRules::merge('tweets', Tweet::$rules, true)
         ]);
+
         if ($validator->fails()) {
             return response()->json(['errors' => $validator->errors()], 422);
         }
+
         $validated = $validator->validated();
 
         $tweets = $validated['tweets'];
+
         foreach ($tweets as $tweet) {
-            if(Tweet::where('id', $tweet['id'])->exists()) {
+            if (Tweet::where('id', $tweet['id'])->exists()) {
                 continue;
             }
-            Tweet::create(['lang' => $validated['lang'], ...$tweet]);
+
+            Tweet::create([
+                'lang' => $validated['lang'],
+                ...$tweet,
+            ]);
         }
+
         return response()->json(['message' => 'success'], 200);
     }
 }
