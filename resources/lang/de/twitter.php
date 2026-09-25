ff5602fa5f09f4eb082fc64a86d07ff15f90bc50
James Morgan <137008809+jamesmvh@users.noreply.github.com>
2026-09-25 07:56:05 +0100

    Fix mobile feed scrolling and remove unused frontend code (#202)
    Co-authored-by: James <james.william.morgan@gmail.com>

--- resources/lang/de/twitter.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-return [
-    'loading' => 'Lade Twitter feed...<br/> Wenn diese Nachricht nicht von alleine verschwindet, überprüfe, ob Skripte in deinem Browser aktiviert sind und lade die Seite neu.<br/>',
-];
